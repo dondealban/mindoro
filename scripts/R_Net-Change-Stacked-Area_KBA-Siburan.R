272b66708258620a9633d5fc2da053584480a0b3
dondealban <dondealban@gmail.com>
2019-08-25 10:19:20 +0800

    Inserted additional columns for missing values in some time-points

--- scripts/R_Net-Change-Stacked-Area_KBA-Siburan.R
+++ scripts/R_Net-Change-Stacked-Area_KBA-Siburan.R
@@ -20,6 +20,12 @@ library(tidyverse)
 # Read csv files in the directory and store as a list
 filenames <- list.files()
 
+# Store files into four separate temporary dataframes 
+period1 <- filenames[1] # 1988
+period2 <- filenames[2] # 2000
+period3 <- filenames[3] # 2010
+period4 <- filenames[4] # 2015
+
 # Function to read data
 readdata <- function(filename) {
   df <- read.csv(filename, sep="\t")
@@ -29,8 +35,20 @@ readdata <- function(filename) {
 }
 
 # Combine as class codes and percentage values in a matrix
-temp <- do.call(rbind, lapply(filenames, readdata))
-colnames(temp) <- c("1","2","3","4","5","6","7","8")
+mat1 <- do.call(rbind, lapply(period1, readdata))
+mat2 <- do.call(rbind, lapply(period2, readdata))
+mat3 <- do.call(rbind, lapply(period3, readdata))
+mat4 <- do.call(rbind, lapply(period4, readdata))
+
+# Create new column with zeroes for Class 6 in 2nd and 3rd period and insert in matrix
+mat1a <- t(as.matrix(c(mat1[,1:ncol(mat1)]))) # transposed 1x7 matrix
+mat2a <- t(as.matrix(c(mat2[,1:5], 0, mat2[,6]))) # transposed 1x7 matrix
+mat3a <- t(as.matrix(c(mat3[,1:5], 0, mat3[,6]))) # transposed 1x7 matrix
+mat4a <- t(as.matrix(c(mat4[,1:ncol(mat4)]))) # transposed 1x7 matrix
+
+# Combine matrices from two periods and change column names
+temp <- rbind(mat1a, mat2a, mat3a, mat4a)
+colnames(temp) <- c("1","2","3","4","5","6","7")
 
 # Add years as another column
 row.names(temp) <- c("1988","2000","2010","2015")
@@ -43,7 +61,6 @@ colnames(data) <- c("Years","Class","Percentage")
 
 plot <- ggplot() + geom_area(aes(x=Years, y=Percentage, fill=factor(Class,
                    labels=c("Forest",
-                            "Mangrove",
                             "Grassland",
                             "Rice Paddy / Bare Soil",
                             "Exposed Rock",
@@ -52,10 +69,10 @@ plot <- ggplot() + geom_area(aes(x=Years, y=Percentage, fill=factor(Class,
                             "Water Body"))), 
                    data=data)
 plot <- plot + labs(x="Year", y="Percentage of Landscape", fill="Land Cover Category")
-plot <- plot + scale_fill_manual(values=c("#246a24","#6666ff","#c6f800","#ffff66",
+plot <- plot + scale_fill_manual(values=c("#246a24","#c6f800","#ffff66",
                                           "#bcbdbc","#07d316","#ff0000","#66ccff"))
 plot <- plot + scale_x_continuous(breaks=c(1988,2000,2010,2015))
 plot <- plot + theme_bw()
 plot <- plot + theme(legend.position="bottom", legend.box="horizontal", legend.title = element_blank())
 
-ggsave(plot, file="StackedArea-00-Mindoro-Island.pdf", width=16, height=15, units="cm", dpi=300)
+ggsave(plot, file="StackedArea-00-Mindoro-KBA-Siburan.pdf", width=16, height=15, units="cm", dpi=300)
