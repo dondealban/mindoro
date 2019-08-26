35112244d1f3549cc5e6cf345536e111cf421457
dondealban <dondealban@gmail.com>
2019-08-26 08:34:13 +0800

    Revised plotting parameters

--- scripts/R_Intensity-Analysis-Category-Level_Combined_v3.R
+++ scripts/R_Intensity-Analysis-Category-Level_Combined_v3.R
@@ -137,4 +137,4 @@ plotCAT <- plotCAT  + theme(legend.position="bottom", legend.box="horizontal", l
 # Save Outputs --------------------------
 
 # Output boxplots to a PDF file
-ggsave(plotCAT, file="Category-Level-Intensity-Analysis_Mindoro.pdf", width=25, height=20, units="cm", dpi=300)
+ggsave(plotCAT, file="Category-Level-Intensity-Analysis_Mindoro_v2.pdf", width=25, height=20, units="cm", dpi=300)
