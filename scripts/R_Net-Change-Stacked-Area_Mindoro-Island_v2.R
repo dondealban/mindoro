f22cd4b859890d20f49ff37845080885dcb2db56
dondealban <dondealban@gmail.com>
2021-04-08 14:03:20 +0800

    Duplicated stacked area script for other sites

--- scripts/R_Net-Change-Stacked-Area_Mindoro-Island_v2.R
+++ scripts/R_Net-Change-Stacked-Area_Mindoro-Island_v2.R
@@ -87,8 +87,8 @@ plot2 <- ggplot() + geom_area(aes(x=Years, y=Percentage, fill=factor(Class,
                              "Shrub / Other Vegetation",
                              "Built-up Area",
                              "Water Body"))), 
-                             data=data2)
-plot2 <- plot2 + labs(title="(a) Mt. Calavite Wildlife Sanctuary", x="Year", y="Percentage of Landscape", fill="Land Cover Category")
+                    data=data2)
+plot2 <- plot2 + labs(title="(b) Mt. Calavite Wildlife Sanctuary", x="Year", y="Percentage of Landscape", fill="Land Cover Category")
 plot2 <- plot2 + scale_fill_manual(values=c("#246a24","#6666ff","#c6f800","#ffff66","#bcbdbc","#07d316","#ff0000","#66ccff"))
 plot2 <- plot2 + scale_x_continuous(breaks=c(1988,2000,2010,2015))
 plot2 <- plot2 + theme_bw()
@@ -96,6 +96,109 @@ plot2 <- plot2 + theme(legend.position="bottom", legend.box="horizontal", legend
 plot2 <- plot2 + theme(legend.text=element_text(size=13))
 plot2 <- plot2 + theme(axis.title=element_text(size=13), axis.text=element_text(size=11))
 plot2 <- plot2 + theme(panel.grid.major=element_blank(), panel.grid.minor=element_blank())
+
+# MT SIBURAN KEY BIODIVERSITY AREA
+
+# Read csv files in the directory and store as a list
+setwd(Dir3)
+filenames3 <- list.files()
+
+# Store files into four separate temporary dataframes 
+period1s <- filenames3[1] # 1988
+period2s <- filenames3[2] # 2000
+period3s <- filenames3[3] # 2010
+period4s <- filenames3[4] # 2015
+
+# Combine as class codes and percentage values in a matrix
+vec1s <- do.call(rbind, lapply(period1s, readdata))
+vec2s <- do.call(rbind, lapply(period2s, readdata))
+vec3s <- do.call(rbind, lapply(period3s, readdata))
+vec4s <- do.call(rbind, lapply(period4s, readdata))
+
+# Create new column with zeroes for Class 6 in 2nd and 3rd period and insert in matrix
+mat1s <- t(as.matrix(c(vec1s[,1:ncol(vec1s)]))) # transposed 1x7 matrix
+mat2s <- t(as.matrix(c(vec2s[,1:5], 0, vec2s[,6]))) # transposed 1x7 matrix
+mat3s <- t(as.matrix(c(vec3s[,1:5], 0, vec3s[,6]))) # transposed 1x7 matrix
+mat4s <- t(as.matrix(c(vec4s[,1:ncol(vec4s)]))) # transposed 1x7 matrix
+
+# Combine matrices from two periods and change column names
+temp3 <- rbind(mat1s, mat2s, mat3s, mat4s)
+colnames(temp3) <- c("1","2","3","4","5","6","7")
+row.names(temp3) <- c("1988","2000","2010","2015") # Add years as another column
+
+# Convert wide format data frame into long format data frame
+data3 <- melt(temp3, id.vars="years", variable.name="class", value.name="percentage")
+colnames(data3) <- c("Years","Class","Percentage")
+
+# Create stacked area plot
+plot3 <- ggplot() + geom_area(aes(x=Years, y=Percentage, fill=factor(Class,
+                    labels=c("Forest",
+                             "Grassland",
+                             "Rice Paddy / Bare Soil",
+                             "Exposed Rock",
+                             "Shrub / Other Vegetation",
+                             "Built-up Area",
+                             "Water Body"))), 
+                    data=data3)
+plot3 <- plot3 + labs(title="(c) Mt. Calavite Wildlife Sanctuary", x="Year", y="Percentage of Landscape", fill="Land Cover Category")
+plot3 <- plot3 + scale_fill_manual(values=c("#246a24","#c6f800","#ffff66",
+                                            "#bcbdbc","#07d316","#ff0000","#66ccff"))
+plot3 <- plot3 + scale_x_continuous(breaks=c(1988,2000,2010,2015))
+plot3 <- plot3 + theme_bw()
+plot3 <- plot3 + theme(legend.position="bottom", legend.box="horizontal", legend.title = element_blank())
+plot3 <- plot3 + theme(legend.text=element_text(size=13))
+plot3 <- plot3 + theme(axis.title=element_text(size=13), axis.text=element_text(size=11))
+plot3 <- plot3 + theme(panel.grid.major=element_blank(), panel.grid.minor=element_blank())
+
+# MTS IGLIT-BACO NATIONAL PARK
+
+# Read csv files in the directory and store as a list
+setwd(Dir4)
+filenames4 <- list.files()
+
+# Store files into four separate temporary dataframes 
+period1b <- filenames4[1] # 1988
+period2b <- filenames4[2] # 2000
+period3b <- filenames4[3] # 2010
+period4b <- filenames4[4] # 2015
+
+# Combine as class codes and percentage values in a matrix
+vec1b <- do.call(rbind, lapply(period1b, readdata))
+vec2b <- do.call(rbind, lapply(period2b, readdata))
+vec3b <- do.call(rbind, lapply(period3b, readdata))
+vec4b <- do.call(rbind, lapply(period4b, readdata))
+
+# Remove unnecessary columns
+mat1b <- t(as.matrix(vec1b[,-c(2,7)]))
+mat2b <- t(as.matrix(vec2b[,-c(2,7)]))
+mat3b <- t(as.matrix(vec3b[,-c(6)]))
+mat4b <- t(as.matrix(vec4b[,-c(6:7)]))
+
+# Combine matrices from two periods and change column names
+temp4 <- rbind(mat1b, mat2b, mat3b, mat4b)
+colnames(temp4) <- c("1","2","3","4","5")
+row.names(temp4) <- c("1988","2000","2010","2015") # Add years as another column
+
+# Convert wide format data frame into long format data frame
+data4 <- melt(temp4, id.vars="years", variable.name="class", value.name="percentage")
+colnames(data4) <- c("Years","Class","Percentage")
+
+# Create stacked area plot
+plot4 <- ggplot() + geom_area(aes(x=Years, y=Percentage, fill=factor(Class,
+                    labels=c("Forest",
+                             "Grassland",
+                             "Rice Paddy / Bare Soil",
+                             "Exposed Rock",
+                             "Shrub / Other Vegetation"))), 
+                    data=data4)
+plot4 <- plot4 + labs(title="(d) Mts. Iglit-Baco National Park", x="Year", y="Percentage of Landscape", fill="Land Cover Category")
+plot4 <- plot4 + scale_fill_manual(values=c("#246a24","#c6f800","#ffff66","#bcbdbc","#07d316"))
+plot4 <- plot4 + scale_x_continuous(breaks=c(1988,2000,2010,2015))
+plot4 <- plot4 + theme_bw()
+plot4 <- plot4 + theme(legend.position="bottom", legend.box="horizontal", legend.title = element_blank())
+plot4 <- plot4 + theme(legend.text=element_text(size=13))
+plot4 <- plot4 + theme(axis.title=element_text(size=13), axis.text=element_text(size=11))
+plot4 <- plot4 + theme(panel.grid.major=element_blank(), panel.grid.minor=element_blank())
 
 
 ggsave(plot, file="StackedArea_Mindoro-Island_v2.pdf", width=16, height=15, units="cm", dpi=300)
